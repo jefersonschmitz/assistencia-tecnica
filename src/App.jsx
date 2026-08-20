2a8aed3c37f1cdf69f2c3cd04fe282dde95fc720
JefersonSchmitz <111818265+jefersonschmitz@users.noreply.github.com>
2026-08-19 23:20:59 -0300

    feito a aba inicial do site

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './components/header'
+import Start from './components/start'
 
 function App() {
   return (
     <>
-      <Header/>     
+      <Header/>   
+      
+      <Start/>
+    
+
     </>
 
   )
---
+++ src/components/start.jsx
@@ -0,0 +1,17 @@
+import './start.css'
+function Start() {
+    return(
+        <section className="start">
+            
+            <img src="/image/start/logo.png" className="start-logo" alt="Logo da empresa" />
+            <h3> Tecnologia que você pode confiar <br/></h3>
+            <p>Soluções rápidas, eficientes e profissionais
+                para manter seus equipamentos sempre funcionando.
+            </p>
+        </section>
+    )
+            
+
+}
+
+export default Start
